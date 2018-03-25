091cf4f0ba1895c9b276d52b6a9420fbdf913b65
sp1di <sp1di96@outlook.com>
2018-03-25 22:03:46 +0200

    New methods in PlayerUtil

---
+++ src/main/java/me/veacty/magiclibrary/ItemStackException.java
@@ -0,0 +1,11 @@
+package me.veacty.magiclibrary;
+
+public class ItemStackException extends RuntimeException {
+
+    public ItemStackException() {
+    }
+
+    public ItemStackException(String text) {
+        super(text);
+    }
+}
--- src/main/java/me/veacty/magiclibrary/utilities/PlayerUtil.java
+++ src/main/java/me/veacty/magiclibrary/utilities/PlayerUtil.java
@@ -1,8 +1,11 @@
 package me.veacty.magiclibrary.utilities;
 
+import me.veacty.magiclibrary.ItemStackException;
+import org.apache.commons.lang.Validate;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
 import java.util.stream.StreamSupport;
 
 public final class PlayerUtil {
@@ -16,6 +19,18 @@ public final class PlayerUtil {
                 StreamSupport.stream(inventory.spliterator(), false)
                         .filter(itemStack::isSimilar)
                         .anyMatch(pickupedItemStack -> pickupedItemStack.getAmount() + itemStack.getAmount() < pickupedItemStack.getMaxStackSize());
+    }
+
+    public static void addItem(Inventory inventory, ItemStack itemStack) {
+
+        if(canPickup(inventory, itemStack)) {
+            inventory.addItem(itemStack);
+        }
+        throw new ItemStackException("the player has no place in the inventory");
+    }
+
+    public static void addItems(Inventory inventory, ItemStack... itemStacks) {
+        Arrays.stream(itemStacks).forEach(itemStack -> addItem(inventory, itemStack));
     }
 
 
